d44f913a645f10560cbec80cf5468cbd7b9055e5
飞花梦影 <582822832@qq.com>
2021-05-06 00:15:10 +0800

    fix

--- dream-tool-core/src/main/java/com/wy/database/Sort.java
+++ dream-tool-core/src/main/java/com/wy/database/Sort.java
@@ -1,5 +1,6 @@
 package com.wy.database;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,30 +8,36 @@ import java.lang.annotation.Target;
 
 import com.wy.enums.Direction;
 
-
 /**
- * 在baseentity中检测是否有排序字段;本注解在{@link com.wy.base.BaseEntity}中使用
+ * 在业务层检测是否有排序字段
  * 
- * @author paradiseWy
- *
+ * @auther 飞花梦影
+ * @date 2021-05-05 23:54:07
+ * @git {@link https://github.com/dreamFlyingFlower}
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Sort {
 
 	/**
-	 * 默认排序字段为实体类字段名,value值代表的字段是驼峰类字段,是数据库字段对应的驼峰字段
+	 * 默认排序字段为实体类字段名.当value有值时,直接使用该值进行数据库查询,不进行转下划线判断
+	 * 
+	 * @return 排序的Java属性名
 	 */
 	String value() default "";
 
 	/**
-	 * 当value为""时,取实体类字段的名称,实体类是否需要转换为蛇形
+	 * 当value为""时,取实体类字段的名称,实体类是否需要转换为蛇形,即下划线
+	 * 
+	 * @return true->转下划线,false->不转下划线
 	 */
 	boolean hump2Snake() default true;
-	
+
 	/**
 	 * 排序升序或降序,默认升序
-	 * @return
+	 * 
+	 * @return 排序类型
 	 */
 	Direction direction() default Direction.ASC;
 }
--- dream-tool-core/src/main/java/com/wy/database/Unique.java
+++ dream-tool-core/src/main/java/com/wy/database/Unique.java
@@ -1,34 +1,49 @@
 package com.wy.database;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @description 当字段上有该注解时表示,字段的值在数据库中唯一
- * @author ParadiseWY
- * @date 2019年7月30日 下午5:21:07
+ * 当字段上有该注解时,表示字段的值在数据库中唯一
+ * 
+ * @author 飞花梦影
+ * @date 2019-07-30 17:21:07
+ * @git {@link https://github.com/dreamFlyingFlower}
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Unique {
+
 	/**
-	 * 数据库字段的名称
+	 * 数据库字段的名称,若为空,默认为Java属性名
+	 * 
+	 * @return 数据库字段名
 	 */
 	String value() default "";
 
 	/**
-	 * 当value为""时,取实体类字段的名称,实体类是否需要转换为蛇形
+	 * 当value为""时,取实体类Java属性名,实体类是否需要转换为蛇形,即下划线模式
+	 * 
+	 * @return 默认true->转下划线,false->不转下划线
 	 */
 	boolean hump2Snake() default true;
 
 	/**
-	 * 当唯一字段进行更新时,前端需要将更新前的该字段的原始值重新传过来,该方法接收原始值
-	 * 1.当该参数有值时,根据反射取该参数在实体类中对应的值,若为null,抛异常
-	 * 2.当该值为null时,将在该注解的字段前添加ori并变成驼峰来查找实体类中的值,如username将变成oriUsername查找
+	 * 定义接收原始值的Java属性名
 	 * 
-	 * @return 修改时的原始值的java接收属性名,该属性名必须在实体类中真实存在
+	 * 当唯一字段进行更新时,需要将注解属性更新前的原始值传入到另外一个指定的Java属性中,另外的Java属性接收原始值
+	 * 
+	 * <pre>
+	 * 1.当该方法不为""时,根据反射取方法返回的Java属性名在实体类中查找.若该Java属性在实体类中没有,抛异常
+	 * 2.当该方法为""时,将在注解Java属性前添加ori并变成驼峰从实体类中查找,如username将变成oriUsername查找.若该Java属性在实体类中没有,抛异常
+	 * 3.接收原始值的Java属性名必须在类中真实存在,否则抛异常
+	 * </pre>
+	 * 
+	 * @return 修改时接收原始值的Java属性名
 	 */
 	String oriName() default "";
 }
