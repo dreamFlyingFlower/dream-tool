4965608bdd4c710cb092b62d262e69279f6c8053
DreamFlyingFlower <yang_wan@bjdv.com>
2023-05-25 18:41:14 +0800

    study something

--- dream-tool-third/src/main/java/com/wy/qrcode/QREncode.java
+++ dream-tool-third/src/main/java/com/wy/qrcode/QREncode.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileSystemView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
@@ -22,6 +23,7 @@ import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.Result;
+import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
@@ -49,6 +51,13 @@ public class QREncode {
 	private static final int WIDTH = 60; // logo宽度
 
 	private static final int HEIGHT = 60; // logo高度
+
+	// FRONT_COLOR:二维码前景色,0x000000 表示黑色
+	private static final int FRONT_COLOR = 0x000000;
+
+	// BACKGROUND_COLOR:二维码背景色,0xFFFFFF 表示白色
+	// 演示用 16 进制表示,和前端页面 CSS 的取色是一样的,注意前后景颜色应该对比明显,如常见的黑白
+	private static final int BACKGROUND_COLOR = 0xFFFFFF;
 
 	/**
 	 * 生成二维码
@@ -141,6 +150,89 @@ public class QREncode {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	public static void createCodeToFile(String content, File codeImgFileSaveDir, String fileName) {
+		try {
+			if (StrTool.isBlank(content) || StrTool.isBlank(fileName)) {
+				return;
+			}
+			content = content.trim();
+			if (codeImgFileSaveDir == null || codeImgFileSaveDir.isFile()) {
+				// 二维码图片存在目录为空,默认放在桌面...
+				codeImgFileSaveDir = FileSystemView.getFileSystemView().getHomeDirectory();
+			}
+			if (!codeImgFileSaveDir.exists()) {
+				// 二维码图片存在目录不存在,开始创建...
+				codeImgFileSaveDir.mkdirs();
+			}
+
+			// 核心代码-生成二维码
+			BufferedImage bufferedImage = getBufferedImage(content);
+
+			File codeImgFile = new File(codeImgFileSaveDir, fileName);
+			ImageIO.write(bufferedImage, "png", codeImgFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 生成二维码并输出到输出流, 通常用于输出到网页上进行显示，输出到网页与输出到磁盘上的文件中，区别在于最后一句 ImageIO.write
+	 * write(RenderedImage im,String formatName,File output)：写到文件中
+	 * write(RenderedImage im,String formatName,OutputStream output)：输出到输出流中
+	 * 
+	 * @param content ：二维码内容
+	 * @param outputStream ：输出流，比如 HttpServletResponse 的 getOutputStream
+	 */
+	public static void createCodeToOutputStream(String content, OutputStream outputStream) {
+		try {
+			if (StrTool.isBlank(content)) {
+				return;
+			}
+			content = content.trim();
+			// 核心代码-生成二维码
+			BufferedImage bufferedImage = getBufferedImage(content);
+
+			// 区别就是这一句，输出到输出流中，如果第三个参数是 File，则输出到文件中
+			ImageIO.write(bufferedImage, "png", outputStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 生成二维码
+	 * 
+	 * @param content
+	 * @return
+	 * @throws WriterException
+	 */
+	private static BufferedImage getBufferedImage(String content) throws WriterException {
+		// com.google.zxing.EncodeHintType：编码提示类型,枚举类型
+		Map<EncodeHintType, Object> hints = new HashMap<>();
+
+		// EncodeHintType.CHARACTER_SET：设置字符编码类型
+		hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+		// EncodeHintType.ERROR_CORRECTION：设置误差校正
+		// ErrorCorrectionLevel：误差校正等级，L = ~7% correction、M = ~15% correction、Q = ~25%
+		// correction、H = ~30% correction
+		// 不设置时，默认为 L 等级，等级不一样，生成的图案不同，但扫描的结果是一样的
+		hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+
+		// EncodeHintType.MARGIN：设置二维码边距，单位像素，值越小，二维码距离四周越近
+		hints.put(EncodeHintType.MARGIN, 1);
+
+		MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+		BitMatrix bitMatrix = multiFormatWriter.encode(content, BarcodeFormat.QR_CODE, WIDTH, HEIGHT, hints);
+		BufferedImage bufferedImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_BGR);
+		for (int x = 0; x < WIDTH; x++) {
+			for (int y = 0; y < HEIGHT; y++) {
+				bufferedImage.setRGB(x, y, bitMatrix.get(x, y) ? FRONT_COLOR : BACKGROUND_COLOR);
+			}
+		}
+		return bufferedImage;
 	}
 
 	/**
