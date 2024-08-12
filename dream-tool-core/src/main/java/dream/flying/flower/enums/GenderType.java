d891d76ac2602ed06ed09d18f31b1fac486546e2
万杨 <yang_wan@bjdv.com>
2024-08-12 18:23:32 +0800

    study something

--- dream-tool-core/src/main/java/dream/flying/flower/enums/GenderType.java
+++ dream-tool-core/src/main/java/dream/flying/flower/enums/GenderType.java
@@ -1,15 +1,25 @@
 package dream.flying.flower.enums;
 
+import java.util.stream.Stream;
+
 import dream.flying.flower.common.PropConverter;
 
 /**
  * 两种方式重写getValue():直接在枚举属性中实现抽象类;在枚举类中统一实现抽象类
  * 
- * @author ParadiseWY
+ * @author 飞花梦影
  * @date 2020-11-23 15:48:40
- * @git {@link https://github.com/mygodness100}
+ * @git {@link https://github.com/dreamFlyingFlower}
  */
-public enum SexEnum implements PropConverter {
+public enum GenderType implements PropConverter {
+
+	UNKNOWN("未知") {
+
+		@Override
+		public Object getValue() {
+			return "未知";
+		}
+	},
 
 	MALE("男") {
 
@@ -25,19 +35,11 @@ public enum SexEnum implements PropConverter {
 		public Object getValue() {
 			return "女";
 		}
-	},
-
-	UNKNOWN("未知") {
-
-		@Override
-		public Object getValue() {
-			return "中性";
-		}
 	};
 
 	private String msg;
 
-	private SexEnum(String msg) {
+	private GenderType(String msg) {
 		this.msg = msg;
 	}
 
@@ -49,5 +51,13 @@ public enum SexEnum implements PropConverter {
 	@Override
 	public String toString() {
 		return this.msg;
+	}
+
+	public static GenderType get(int code) {
+		return Stream.of(values()).filter(t -> t.ordinal() == code).findFirst().orElse(null);
+	}
+
+	public static GenderType get(String code) {
+		return Stream.of(values()).filter(t -> t.name().equalsIgnoreCase(code)).findFirst().orElse(null);
 	}
 }
---
+++ dream-tool-core/src/main/java/dream/flying/flower/enums/IdCardType.java
@@ -0,0 +1,44 @@
+package dream.flying.flower.enums;
+
+import java.util.stream.Stream;
+
+import dream.flying.flower.common.StatusMsg;
+
+/**
+ * 身份标识类型
+ * 
+ * @author 飞花梦影
+ * @date 2024-07-30 21:34:56
+ * @git {@link https://github.com/dreamFlyingFlower}
+ */
+public enum IdCardType implements StatusMsg {
+
+	/** 未知 */
+	UNKNOWN,
+	/** 身份证 */
+	ID_CARD,
+	/** 护照 */
+	PASSPORT,
+	/** 学生证 */
+	STUDENT_CARD,
+	/** 军人证 */
+	MILITARY_CARD;
+
+	@Override
+	public String getMsg() {
+		return name();
+	}
+
+	@Override
+	public Integer getCode() {
+		return ordinal();
+	}
+
+	public static IdCardType get(int code) {
+		return Stream.of(values()).filter(t -> t.ordinal() == code).findFirst().orElse(null);
+	}
+
+	public static IdCardType get(String code) {
+		return Stream.of(values()).filter(t -> t.name().equalsIgnoreCase(code)).findFirst().orElse(null);
+	}
+}
