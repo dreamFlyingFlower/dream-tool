50ccb9106ec279ca2052dbd82d2fcb9fa1ee3dd9
万杨 <yang_wan@bjdv.com>
2024-11-06 18:08:08 +0800

    study something

---
+++ dream-tool-core/src/main/java/dream/flying/flower/annotation/Unused.java
@@ -0,0 +1,21 @@
+package dream.flying.flower.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 一个标记注解,表示被标记的类或对象暂未使用
+ *
+ * @author 飞花梦影
+ * @date 2021-03-11 23:23:31
+ * @git {@link <a href="https://github.com/dreamFlyingFlower">...</a>}
+ */
+@Documented
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Unused {
+
+}
