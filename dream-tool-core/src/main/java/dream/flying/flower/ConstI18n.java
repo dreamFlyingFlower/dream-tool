1992a712a4a4abd415c201718c45e37845ed8eaf
万杨 <yang_wan@bjdv.com>
2024-11-19 20:01:02 +0800

    study something

--- dream-tool-core/src/main/java/dream/flying/flower/ConstI18n.java
+++ dream-tool-core/src/main/java/dream/flying/flower/ConstI18n.java
@@ -9,137 +9,137 @@ package dream.flying.flower;
  */
 public interface ConstI18n {
 
-	String MSG_FAIL = "msg_fail";
+	String MSG_FAIL = "msg.fail";
 
-	String MSG_SUCCESS = "msg_success";
+	String MSG_SUCCESS = "msg.success";
 
-	String MSG_UNKNOWN = "msg_unknown";
+	String MSG_UNKNOWN = "msg.unknown";
 
-	String MSG_INTERNAL_UNAUTHORIZED = "msg_internal_unauthorized";
+	String MSG_INTERNAL_UNAUTHORIZED = "msg.internal.unauthorized";
 
-	String MSG_INTERNAL_FORBIDDEN = "msg_internal_forbidden";
+	String MSG_INTERNAL_FORBIDDEN = "msg.internal.forbidden";
 
-	String MSG_INTERNAL_SERVER_ERROR = "msg_internal_server_error";
+	String MSG_INTERNAL_SERVER_ERROR = "msg.internal.server.error";
 
-	String MSG_LOGIN_SUCCESS_LOGIN = "msg_login_success_login";
+	String MSG_LOGIN_SUCCESS_LOGIN = "msg.login.success.login";
 
-	String MSG_LOGIN_SUCCESS_LOGOUT = "msg_login_success_logout";
+	String MSG_LOGIN_SUCCESS_LOGOUT = "msg.login.success.logout";
 
-	String MSG_LOGIN_FAIL = "msg_login_fail";
+	String MSG_LOGIN_FAIL = "msg.login.fail";
 
-	String MSG_LOGIN_FAIL_USERNAME = "msg_login_fail_username";
+	String MSG_LOGIN_FAIL_USERNAME = "msg.login.fail.username";
 
-	String MSG_LOGIN_FAIL_PASSWORD = "msg_login_fail_password";
+	String MSG_LOGIN_FAIL_PASSWORD = "msg.login.fail.password";
 
-	String MSG_LOGIN_FAIL_TIMEOUT = "msg_login_fail_timeout";
+	String MSG_LOGIN_FAIL_TIMEOUT = "msg.login.fail.timeout";
 
-	String MSG_LOGIN_FAIL_NOT_LOGIN = "msg_login_fail_not_login";
+	String MSG_LOGIN_FAIL_NOT_LOGIN = "msg.login.fail.not.login";
 
-	String MSG_LOGIN_CAPTCHA_ERROR = "msg_login_captcha_error";
+	String MSG_LOGIN_CAPTCHA_ERROR = "msg.login.captcha.error";
 
-	String MSG_LOGIN_CAPTCHA_EMPTY = "msg_login_captcha_empty";
+	String MSG_LOGIN_CAPTCHA_EMPTY = "msg.login.captcha.empty";
 
-	String MSG_LOGIN_CAPTCHA_NOT_EXIST = "msg_login_captcha_not_exist";
+	String MSG_LOGIN_CAPTCHA_NOT_EXIST = "msg.login.captcha.not.exist";
 
-	String MSG_LOGIN_CAPTCHA_ILLEGAL = "msg_login_captcha_illegal";
+	String MSG_LOGIN_CAPTCHA_ILLEGAL = "msg.login.captcha.illegal";
 
-	String MSG_USER_NOT_EXIST = "msg_user_not_exist";
+	String MSG_USER_NOT_EXIST = "msg.user.not.exist";
 
-	String MSG_USER_NOT_DISTRIBUTE_ROLE = "msg_user_not_distribute_role";
+	String MSG_USER_NOT_DISTRIBUTE_ROLE = "msg.user.not.distribute.role";
 
-	String MSG_USER_NOT_AVALIABLE = "msg_user_not_avaliable";
+	String MSG_USER_NOT_AVALIABLE = "msg.user.not.avaliable";
 
-	String MSG_USER_IS_BLACK = "msg_user_is_black";
+	String MSG_USER_IS_BLACK = "msg.user.is.black";
 
-	String MSG_USER_LOCK = "msg_user_lock";
+	String MSG_USER_LOCK = "msg.user.lock";
 
-	String MSG_USER_PWD_ORIGINAL_ERROR = "msg_user_pwd_original_error";
+	String MSG_USER_PWD_ORIGINAL_ERROR = "msg.user.pwd.original.error";
 
-	String MSG_USER_PWD_OLD_NOT_SAME_NEW = "msg_user_pwd_old_not_same_new";
+	String MSG_USER_PWD_OLD_NOT_SAME_NEW = "msg.user.pwd.old.not.same.new";
 
-	String MSG_AUTH_EMPTY = "msg_auth_empty";
+	String MSG_AUTH_EMPTY = "msg.auth.empty";
 
-	String MSG_AUTH_FAIL = "msg_auth_fail";
+	String MSG_AUTH_FAIL = "msg.auth.fail";
 
-	String MSG_AUTH_DENIED = "msg_auth_denied";
+	String MSG_AUTH_DENIED = "msg.auth.denied";
 
-	String MSG_AUTH_TOKEN_ERROR = "msg_auth_token_error";
+	String MSG_AUTH_TOKEN_ERROR = "msg.auth.token.error";
 
-	String MSG_AUTH_TOKEN_EMPTY = "msg_auth_token_empty";
+	String MSG_AUTH_TOKEN_EMPTY = "msg.auth.token.empty";
 
-	String MSG_AUTH_TOKEN_NOT_EXIST = "msg_auth_token_not_exist";
+	String MSG_AUTH_TOKEN_NOT_EXIST = "msg.auth.token.not.exist";
 
-	String MSG_AUTH_TOKEN_ILLEGAL = "msg_auth_token_illegal";
+	String MSG_AUTH_TOKEN_ILLEGAL = "msg.auth.token.illegal";
 
-	String MSG_ROLE_NOT_EXIST = "msg_role_not_exist";
+	String MSG_ROLE_NOT_EXIST = "msg.role.not.exist";
 
-	String MSG_ROLE_NOT_OPERATE_ADMIN = "msg_role_not_operate_admin";
+	String MSG_ROLE_NOT_OPERATE_ADMIN = "msg.role.not.operate.admin";
 
-	String MSG_ROLE_PERMISSION_DENIED = "msg_role_permission_denied";
+	String MSG_ROLE_PERMISSION_DENIED = "msg.role.permission.denied";
 
-	String MSG_ROLE_UNASSIGNED_RESOURCE = "msg_role_unassigned_resource";
+	String MSG_ROLE_UNASSIGNED_RESOURCE = "msg.role.unassigned.resource";
 
-	String MSG_PARAM_ERROR = "msg_param_error";
+	String MSG_PARAM_ERROR = "msg.param.error";
 
-	String MSG_PARAM_NOT_NULL = "msg_param_not_null";
+	String MSG_PARAM_NOT_NULL = "msg.param.not.null";
 
-	String MSG_PARAM_INVALID = "msg_param_invalid";
+	String MSG_PARAM_INVALID = "msg.param.invalid";
 
-	String MSG_PARAM_REQUIRED_IS_NULL = "msg_param_required_is_null";
+	String MSG_PARAM_REQUIRED_IS_NULL = "msg.param.required.is.null";
 
-	String MSG_DB_ERROR = "msg_db_error";
+	String MSG_DB_ERROR = "msg.db.error";
 
-	String MSG_DB_CREATE_FAIL = "msg_db_create_fail";
+	String MSG_DB_CREATE_FAIL = "msg.db.create.fail";
 
-	String MSG_DB_DELETE_FAIL = "msg_db_delete_fail";
+	String MSG_DB_DELETE_FAIL = "msg.db.delete.fail";
 
-	String MSG_DB_MODIFY_FAIL = "msg_db_modify_fail";
+	String MSG_DB_MODIFY_FAIL = "msg.db.modify.fail";
 
-	String MSG_DB_QUERY_FAIL = "msg_db_query_fail";
+	String MSG_DB_QUERY_FAIL = "msg.db.query.fail";
 
-	String MSG_DB_DUPLICATE_KEY_ERROR = "msg_db_duplicate_key_error";
+	String MSG_DB_DUPLICATE_KEY_ERROR = "msg.db.duplicate.key.error";
 
-	String MSG_DB_DATA_EXIST = "msg_db_data_exist";
+	String MSG_DB_DATA_EXIST = "msg.db.data.exist";
 
-	String MSG_DB_DATA_NONE = "msg_db_data_none";
+	String MSG_DB_DATA_NONE = "msg.db.data.none";
 
-	String MSG_FILE_NOT_EXIST = "msg_file_not_exist";
+	String MSG_FILE_NOT_EXIST = "msg.file.not.exist";
 
-	String MSG_FILE_DIRECTORY_NOT_EXIST = "msg_file_directory_not_exist";
+	String MSG_FILE_DIRECTORY_NOT_EXIST = "msg.file.directory.not.exist";
 
-	String MSG_FILE_NOT_NULL = "msg_file_not_null";
+	String MSG_FILE_NOT_NULL = "msg.file.not.null";
 
-	String MSG_FILE_DIRECTORY_NOT_NULL = "msg_file_directory_not_null";
+	String MSG_FILE_DIRECTORY_NOT_NULL = "msg.file.directory.not.null";
 
-	String MSG_FILE_UPLOAD_FAILED = "msg_file_upload_failed";
+	String MSG_FILE_UPLOAD_FAILED = "msg.file.upload.failed";
 
-	String MSG_FILE_DOWNLOAD_FAILED = "msg_file_download_failed";
+	String MSG_FILE_DOWNLOAD_FAILED = "msg.file.download.failed";
 
-	String MSG_MAIL_USERNAME_NULL = "msg_mail_username_null";
+	String MSG_MAIL_USERNAME_NULL = "msg.mail.username.null";
 
-	String MSG_MAIL_PASSWORD_NULL = "msg_mail_password_null";
+	String MSG_MAIL_PASSWORD_NULL = "msg.mail.password.null";
 
-	String MSG_MAIL_FROM_NULL = "msg_mail_from_null";
+	String MSG_MAIL_FROM_NULL = "msg.mail.from.null";
 
-	String MSG_MAIL_TO_NULL = "msg_mail_to_null";
+	String MSG_MAIL_TO_NULL = "msg.mail.to.null";
 
-	String MSG_MAIL_CONTENT_NULL = "msg_mail_content_null";
+	String MSG_MAIL_CONTENT_NULL = "msg.mail.content.null";
 
-	String MSG_SYS_ERROR = "msg_sys_error";
+	String MSG_SYS_ERROR = "msg.sys.error";
 
-	String MSG_SYS_MAINTAIN = "msg_sys_maintain";
+	String MSG_SYS_MAINTAIN = "msg.sys.maintain";
 
-	String MSG_SYS_BUSY = "msg_sys_busy";
+	String MSG_SYS_BUSY = "msg.sys.busy";
 
-	String MSG_TIMESTAMP_NULL = "msg_timestamp_null";
+	String MSG_TIMESTAMP_NULL = "msg.timestamp.null";
 
-	String MSG_TIMESTAMP_ERROR = "msg_timestamp_error";
+	String MSG_TIMESTAMP_ERROR = "msg.timestamp.error";
 
-	String MSG_REQUEST_URL_NOT_EXIST = "msg_request_url_not_exist";
+	String MSG_REQUEST_URL_NOT_EXIST = "msg.request.url.not.exist";
 
-	String MSG_REQUEST_HTTP_METHOD_NOT_SUPPORTED = "msg_request_http_method_not_supported";
+	String MSG_REQUEST_HTTP_METHOD_NOT_SUPPORTED = "msg.request.http.method.not.supported";
 
-	String MSG_REQUEST_MEDIA_TYPE_NOT_SUPPORTED = "msg_request_media_type_not_supported";
+	String MSG_REQUEST_MEDIA_TYPE_NOT_SUPPORTED = "msg.request.media.type.not.supported";
 
-	String MSG_REQUEST_TIME_OUT = "msg_request_time_out";
+	String MSG_REQUEST_TIME_OUT = "msg.request.time.out";
 }
--- dream-tool-core/src/main/java/dream/flying/flower/ConstI18nFormat.java
+++ dream-tool-core/src/main/java/dream/flying/flower/ConstI18nFormat.java
@@ -9,113 +9,113 @@ package dream.flying.flower;
  */
 public interface ConstI18nFormat {
 
-	String MSG_FORMAT_LOGIN_SUCCESS_LOGIN = "msg_format_login_success_login";
+	String MSG_FORMAT_LOGIN_SUCCESS_LOGIN = "msg.format.login.success.login";
 
-	String MSG_FORMAT_LOGIN_SUCCESS_LOGOUT = "msg_format_login_success_logout";
+	String MSG_FORMAT_LOGIN_SUCCESS_LOGOUT = "msg.format.login.success.logout";
 
-	String MSG_FORMAT_LOGIN_FAIL = "msg_format_login_fail";
+	String MSG_FORMAT_LOGIN_FAIL = "msg.format.login.fail";
 
-	String MSG_FORMAT_LOGIN_FAIL_USERNAME = "msg_format_login_fail_username";
+	String MSG_FORMAT_LOGIN_FAIL_USERNAME = "msg.format.login.fail.username";
 
-	String MSG_FORMAT_LOGIN_FAIL_PASSWORD = "msg_format_login_fail_password";
+	String MSG_FORMAT_LOGIN_FAIL_PASSWORD = "msg.format.login.fail.password";
 
-	String MSG_FORMAT_LOGIN_FAIL_TIMEOUT = "msg_format_login_fail_timeout";
+	String MSG_FORMAT_LOGIN_FAIL_TIMEOUT = "msg.format.login.fail.timeout";
 
-	String MSG_FORMAT_LOGIN_FAIL_NOT_LOGIN = "msg_format_login_fail_not_login";
+	String MSG_FORMAT_LOGIN_FAIL_NOT_LOGIN = "msg.format.login.fail.not.login";
 
-	String MSG_FORMAT_LOGIN_CAPTCHA_ERROR = "msg_format_login_captcha_error";
+	String MSG_FORMAT_LOGIN_CAPTCHA_ERROR = "msg.format.login.captcha.error";
 
-	String MSG_FORMAT_LOGIN_CAPTCHA_EMPTY = "msg_format_login_captcha_empty";
+	String MSG_FORMAT_LOGIN_CAPTCHA_EMPTY = "msg.format.login.captcha.empty";
 
-	String MSG_FORMAT_LOGIN_CAPTCHA_NOT_EXIST = "msg_format_login_captcha_not_exist";
+	String MSG_FORMAT_LOGIN_CAPTCHA_NOT_EXIST = "msg.format.login.captcha.not.exist";
 
-	String MSG_FORMAT_LOGIN_CAPTCHA_ILLEGAL = "msg_format_login_captcha_illegal";
+	String MSG_FORMAT_LOGIN_CAPTCHA_ILLEGAL = "msg.format.login.captcha.illegal";
 
-	String MSG_FORMAT_USER_NOT_EXIST = "msg_format_user_not_exist";
+	String MSG_FORMAT_USER_NOT_EXIST = "msg.format.user.not.exist";
 
-	String MSG_FORMAT_USER_NOT_DISTRIBUTE_ROLE = "msg_format_user_not_distribute_role";
+	String MSG_FORMAT_USER_NOT_DISTRIBUTE_ROLE = "msg.format.user.not.distribute.role";
 
-	String MSG_FORMAT_USER_NOT_AVALIABLE = "msg_format_user_not_avaliable";
+	String MSG_FORMAT_USER_NOT_AVALIABLE = "msg.format.user.not.avaliable";
 
-	String MSG_FORMAT_USER_IS_BLACK = "msg_format_user_is_black";
+	String MSG_FORMAT_USER_IS_BLACK = "msg.format.user.is.black";
 
-	String MSG_FORMAT_USER_LOCK = "msg_format_user_lock";
+	String MSG_FORMAT_USER_LOCK = "msg.format.user.lock";
 
-	String MSG_FORMAT_USER_PWD_ORIGINAL_ERROR = "msg_format_user_pwd_original_error";
+	String MSG_FORMAT_USER_PWD_ORIGINAL_ERROR = "msg.format.user.pwd.original.error";
 
-	String MSG_FORMAT_USER_PWD_OLD_NOT_SAME_NEW = "msg_format_user_pwd_old_not_same_new";
+	String MSG_FORMAT_USER_PWD_OLD_NOT_SAME_NEW = "msg.format.user.pwd.old.not.same.new";
 
-	String MSG_FORMAT_AUTH_EMPTY = "msg_format_auth_empty";
+	String MSG_FORMAT_AUTH_EMPTY = "msg.format.auth.empty";
 
-	String MSG_FORMAT_AUTH_FAIL = "msg_format_auth_fail";
+	String MSG_FORMAT_AUTH_FAIL = "msg.format.auth.fail";
 
-	String MSG_FORMAT_AUTH_DENIED = "msg_format_auth_denied";
+	String MSG_FORMAT_AUTH_DENIED = "msg.format.auth.denied";
 
-	String MSG_FORMAT_AUTH_TOKEN_ERROR = "msg_format_auth_token_error";
+	String MSG_FORMAT_AUTH_TOKEN_ERROR = "msg.format.auth.token.error";
 
-	String MSG_FORMAT_AUTH_TOKEN_EMPTY = "msg_format_auth_token_empty";
+	String MSG_FORMAT_AUTH_TOKEN_EMPTY = "msg.format.auth.token.empty";
 
-	String MSG_FORMAT_AUTH_TOKEN_NOT_EXIST = "msg_format_auth_token_not_exist";
+	String MSG_FORMAT_AUTH_TOKEN_NOT_EXIST = "msg.format.auth.token.not.exist";
 
-	String MSG_FORMAT_AUTH_TOKEN_ILLEGAL = "msg_format_auth_token_illegal";
+	String MSG_FORMAT_AUTH_TOKEN_ILLEGAL = "msg.format.auth.token.illegal";
 
-	String MSG_FORMAT_ROLE_NOT_EXIST = "msg_format_role_not_exist";
+	String MSG_FORMAT_ROLE_NOT_EXIST = "msg.format.role.not.exist";
 
-	String MSG_FORMAT_ROLE_NOT_OPERATE_ADMIN = "msg_format_role_not_operate_admin";
+	String MSG_FORMAT_ROLE_NOT_OPERATE_ADMIN = "msg.format.role.not.operate.admin";
 
-	String MSG_FORMAT_ROLE_PERMISSION_DENIED = "msg_format_role_permission_denied";
+	String MSG_FORMAT_ROLE_PERMISSION_DENIED = "msg.format.role.permission.denied";
 
-	String MSG_FORMAT_ROLE_UNASSIGNED_RESOURCE = "msg_format_role_unassigned_resource";
+	String MSG_FORMAT_ROLE_UNASSIGNED_RESOURCE = "msg.format.role.unassigned.resource";
 
-	String MSG_FORMAT_PARAM_REQUIRED_IS_NULL = "msg_format_param_required_is_null";
+	String MSG_FORMAT_PARAM_REQUIRED_IS_NULL = "msg.format.param.required.is.null";
 
-	String MSG_FORMAT_DB_ERROR = "msg_format_db_error";
+	String MSG_FORMAT_DB_ERROR = "msg.format.db.error";
 
-	String MSG_FORMAT_DB_CREATE_FAIL = "msg_format_db_create_fail";
+	String MSG_FORMAT_DB_CREATE_FAIL = "msg.format.db.create.fail";
 
-	String MSG_FORMAT_DB_DELETE_FAIL = "msg_format_db_delete_fail";
+	String MSG_FORMAT_DB_DELETE_FAIL = "msg.format.db.delete.fail";
 
-	String MSG_FORMAT_DB_MODIFY_FAIL = "msg_format_db_modify_fail";
+	String MSG_FORMAT_DB_MODIFY_FAIL = "msg.format.db.modify.fail";
 
-	String MSG_FORMAT_DB_QUERY_FAIL = "msg_format_db_query_fail";
+	String MSG_FORMAT_DB_QUERY_FAIL = "msg.format.db.query.fail";
 
-	String MSG_FORMAT_DB_DUPLICATE_KEY_ERROR = "msg_format_db_duplicate_key_error";
+	String MSG_FORMAT_DB_DUPLICATE_KEY_ERROR = "msg.format.db.duplicate.key.error";
 
-	String MSG_FORMAT_FILE_NOT_EXIST = "msg_format_file_not_exist";
+	String MSG_FORMAT_FILE_NOT_EXIST = "msg.format.file.not.exist";
 
-	String MSG_FORMAT_FILE_DIRECTORY_NOT_EXIST = "msg_format_file_directory_not_exist";
+	String MSG_FORMAT_FILE_DIRECTORY_NOT_EXIST = "msg.format.file.directory.not.exist";
 
-	String MSG_FORMAT_FILE_NOT_NULL = "msg_format_file_not_null";
+	String MSG_FORMAT_FILE_NOT_NULL = "msg.format.file.not.null";
 
-	String MSG_FORMAT_FILE_DIRECTORY_NOT_NULL = "msg_format_file_directory_not_null";
+	String MSG_FORMAT_FILE_DIRECTORY_NOT_NULL = "msg.format.file.directory.not.null";
 
-	String MSG_FORMAT_FILE_UPLOAD_FAILED = "msg_format_file_upload_failed";
+	String MSG_FORMAT_FILE_UPLOAD_FAILED = "msg.format.file.upload.failed";
 
-	String MSG_FORMAT_FILE_DOWNLOAD_FAILED = "msg_format_file_download_failed";
+	String MSG_FORMAT_FILE_DOWNLOAD_FAILED = "msg.format.file.download.failed";
 
-	String MSG_FORMAT_MAIL_USERNAME_NULL = "msg_format_mail_username_null";
+	String MSG_FORMAT_MAIL_USERNAME_NULL = "msg.format.mail.username.null";
 
-	String MSG_FORMAT_MAIL_PASSWORD_NULL = "msg_format_mail_password_null";
+	String MSG_FORMAT_MAIL_PASSWORD_NULL = "msg.format.mail.password.null";
 
-	String MSG_FORMAT_MAIL_FROM_NULL = "msg_format_mail_from_null";
+	String MSG_FORMAT_MAIL_FROM_NULL = "msg.format.mail.from.null";
 
-	String MSG_FORMAT_MAIL_TO_NULL = "msg_format_mail_to_null";
+	String MSG_FORMAT_MAIL_TO_NULL = "msg.format.mail.to.null";
 
-	String MSG_FORMAT_MAIL_CONTENT_NULL = "msg_format_mail_content_null";
+	String MSG_FORMAT_MAIL_CONTENT_NULL = "msg.format.mail.content.null";
 
-	String MSG_FORMAT_SYS_ERROR = "msg_format_sys_error";
+	String MSG_FORMAT_SYS_ERROR = "msg.format.sys.error";
 
-	String MSG_FORMAT_SYS_MAINTAIN = "msg_format_sys_maintain";
+	String MSG_FORMAT_SYS_MAINTAIN = "msg.format.sys.maintain";
 
-	String MSG_FORMAT_SYS_BUSY = "msg_format_sys_busy";
+	String MSG_FORMAT_SYS_BUSY = "msg.format.sys.busy";
 
-	String MSG_FORMAT_TIMESTAMP_NULL = "msg_format_timestamp_null";
+	String MSG_FORMAT_TIMESTAMP_NULL = "msg.format.timestamp.null";
 
-	String MSG_FORMAT_TIMESTAMP_ERROR = "msg_format_timestamp_error";
+	String MSG_FORMAT_TIMESTAMP_ERROR = "msg.format.timestamp.error";
 
-	String MSG_FORMAT_REQUEST_URL_NOT_EXIST = "msg_format_request_url_not_exist";
+	String MSG_FORMAT_REQUEST_URL_NOT_EXIST = "msg.format.request.url.not.exist";
 
-	String MSG_FORMAT_REQUEST_HTTP_METHOD_NOT_SUPPORTED = "msg_format_request_http_method_not_supported";
+	String MSG_FORMAT_REQUEST_HTTP_METHOD_NOT_SUPPORTED = "msg.format.request.http.method.not.supported";
 
-	String MSG_FORMAT_REQUEST_MEDIA_TYPE_NOT_SUPPORTED = "msg_format_request_media_type_not_supported";
+	String MSG_FORMAT_REQUEST_MEDIA_TYPE_NOT_SUPPORTED = "msg.format.request.media.type.not.supported";
 }
